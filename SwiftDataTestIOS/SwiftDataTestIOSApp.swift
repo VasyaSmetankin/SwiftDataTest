023ac9acb6a91ad2ec875d76ce54a81a4466ec69
VasyaSmetankin <medvedevkon70@gmail.com>
2024-04-29 17:08:15 +0300

    Initial Commit

---
+++ SwiftDataTestIOS/SwiftDataTestIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataTestIOSApp.swift
+//  SwiftDataTestIOS
+//
+//  Created by Vasya Smetankin on 29.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataTestIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
